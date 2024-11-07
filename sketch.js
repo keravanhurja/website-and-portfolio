7417991a8e8170f03409d96301c0b114bfa9deba
keravanhurja <rullapoika@gmail.com>
2024-11-07 18:57:59 +0200

    v0.008

--- sketch.js
+++ sketch.js
@@ -1,3 +1,11 @@
+const hamburger = document.querySelector('.hamburger');
+const menuItems = document.querySelector('.menuItems');
+
+hamburger.addEventListener('click', () => {
+  menuItems.classList.toggle('active');
+  hamburger.classList.toggle('active'); //this is for the hamburger icon animation
+});
+
 function setup() {
   let myCanvas = createCanvas(50, 50);
   myCanvas.parent('canvas')
