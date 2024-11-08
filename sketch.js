5288532dcb493e3f3de0dea7ac91b27af4622b3d
keravanhurja <rullapoika@gmail.com>
2024-11-08 15:40:27 +0200

    v0.012

--- sketch.js
+++ sketch.js
@@ -1,7 +1,9 @@
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav-links');
+if (window.innerWidth <= 768){
+  const hamburger = document.querySelector('.hamburger');
+  const navLinks = document.querySelector('.nav-links');
 
-hamburger.addEventListener('click', () => {
+  hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('active'); // Slide in/out the menu
     hamburger.classList.toggle('active'); // Animate hamburger icon to "X"
-});
+  });
+}
