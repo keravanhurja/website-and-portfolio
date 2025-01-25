e7afc50b58c69f109f8a52c01fc238326ce11c6b
keravanhurja <rullapoika@gmail.com>
2025-01-25 18:30:46 +0200

    version 0.1

--- sketch.js
+++
@@ -1,9 +0,0 @@
-if (window.innerWidth <= 768){
-  const hamburger = document.querySelector('.hamburger');
-  const navLinks = document.querySelector('.nav-links');
-
-  hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('active'); // Slide in/out the menu
-    hamburger.classList.toggle('active'); // Animate hamburger icon to "X"
-  });
-}
